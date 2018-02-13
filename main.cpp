a350cd1509470636b757c1603ca6c0e7119e13e4
cristinapereez <cperez58@mail.csuchico.edu>
2018-02-13 12:12:37 -0800

    create lucky number

--- main.cpp
+++ main.cpp
@@ -9,14 +9,17 @@ int main()
   //declare variables
 string first;
 string last;
+int lucky;
   //get user input
 cout<<"What is your first name?\n";
 cin>>first;
-cout<<"What is yourirst[0] last name?\n";
+cout<<"What is your last name?\n";
 cin>>last;
 cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
 
   //tell fortune
+  lucky = first.length();
+cout<<"Your lucky number is "<<lucky<<endl;
 
   return 0;
 }
