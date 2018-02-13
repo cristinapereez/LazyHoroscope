0281aa6455a0cda0e681e0b6d34d66bddfc3fa3d
cristinapereez <cperez58@mail.csuchico.edu>
2018-02-13 11:58:40 -0800

    create initials

--- main.cpp
+++ main.cpp
@@ -9,12 +9,13 @@ int main()
   //declare variables
 string first;
 string last;
-
   //get user input
 cout<<"What is your first name?\n";
 cin>>first;
-cout<<"What is your last name?\n";
+cout<<"What is yourirst[0] last name?\n";
 cin>>last;
+cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
+
   //tell fortune
 
   return 0;
