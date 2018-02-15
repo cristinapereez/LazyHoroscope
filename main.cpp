bb3b0a1a7b136884d5068bb2182ef4e22dc77904
cristinapereez <cperez58@mail.csuchico.edu>
2018-02-15 12:05:49 -0800

    Tell future

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
 string first;
 string last;
 int lucky;
+
   //get user input
 cout<<"What is your first name?\n";
 cin>>first;
@@ -19,7 +20,23 @@ cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
 
   //tell fortune
   lucky = first.length();
+  char letter = first[0];
+  
 cout<<"Your lucky number is "<<lucky<<endl;
+if(letter == 'a'||letter == 'A'||letter == 'e'||letter == 'E'||letter=='I'||letter=='i'||letter=='o'||letter=='O'||letter=='u'||letter=='U')
+{
+cout<<"You are destined to be famous!"<<endl;
+}
+else
+{ 
+cout<<"You should keep a low profile"<<endl;
+}
+char letter2 = last[last.length()-1];
+if(letter2 == 'a'||letter2=='e'||letter2 == 'i'||letter=='o'||letter=='u')
+{
+cout<<"You have already met your true love."<<endl;
+}
+cout<<"Have a good day!"<<endl;
 
   return 0;
 }
